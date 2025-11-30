b5ceb66903d364184756b9bf2fd839b221e42792
fracozzi <francesco.cozzi@centai.eu>
2025-11-30 20:56:18 +0100

    Diffusion-only ablated model as used in the paper

--- models/surrogate/nnmodel_diffusion_only.py
+++ models/surrogate/nnmodel_diffusion_only.py
@@ -2,15 +2,22 @@ from models.abm.abm import ABM, ABMFeaturizer
 from models.abm.pp_functions import update_parental
 
 import numpy as np
+import pandas as pd
 import random
+import matplotlib.pyplot as plt
 import torch
+from torch import nn, einsum
 import torch.nn.functional as F
-from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
 from torch.optim import Adam
 from tqdm.auto import tqdm 
+from einops import rearrange
+import seaborn as sns
+import torch_geometric as tg
 from torch_geometric.nn import MessagePassing
+import networkx as nx
 from tqdm import tqdm
-from torch_geometric.data import Data, DataLoader
+import time
 
 
 # Utility functions 
@@ -18,132 +25,379 @@ from torch_geometric.data import Data, DataLoader
 def exists(x):
     return x is not None
 
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight)
+        # m.bias.data.fill_(0.01)
+
+def extract(a, t, x_shape):
+    batch_size = t.shape[0]
+    out = a.gather(-1, t.cpu())
+    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
+
 # Model components definitions
 
-class GNN(MessagePassing):
-    def __init__(self, in_channels, out_channels, hidden=[8,16,32],aggregation='add'):
-        super().__init__(aggr=aggregation)
-        
-        self.dims = list(zip(
-                    [2 * in_channels] + hidden, # 2 is one local one neighbors
-                    hidden + [out_channels])
-                        )
-
-        self.layers = nn.ModuleList([])
-        for dim_in, dim_out in self.dims:
-            self.layers.append(
-                nn.Sequential(
-                nn.Linear(dim_in,dim_out),
-                nn.LeakyReLU(negative_slope=0.1)
-                )
-            )
-               
-    def forward(self,x,edge_index):
-
-        messages = self.propagate(edge_index, x=x)
-        h = torch.hstack([messages,x])
-
-        for block in self.layers:
-            h = block(h)
-        return h
+class SinusoidalTimeEmbeddings(nn.Module):
     
-    def message(self, x_j):
-        return x_j
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, time):
+        device = time.device
+        
+        half_dim = self.dim // 2
+        if half_dim == 0:
+            half_dim = self.dim
+           
+        embeddings = np.log(10000) / half_dim
+        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), - embeddings.cos()), dim=-1)
+        
+        return embeddings
+    
+class Block(nn.Module):
+    
+    def __init__(self,dim_in,dim_out):
+        super().__init__()
+        self.lin = nn.Linear(dim_in,dim_out)
+        self.act = nn.LeakyReLU(negative_slope=0.1)
+
+    def forward(self,x):               
+        x = self.lin(x)
+        x = self.act(x)
+        return x
+    
+class NetBlock(nn.Module):
+    
+    def __init__(self,dim_in,dim_out,*,time_emb_dim=None,state_dim=None,res_unit=False,norm=False):
+        super().__init__()
+        
+        self.block1 = Block(dim_in, dim_out)
+        self.block2 = Block(dim_out,dim_out)
+        
+        if exists(time_emb_dim):
+            self.time_mlp = nn.Sequential(nn.LeakyReLU(negative_slope=0.1),nn.Linear(time_emb_dim,dim_out))
+        else:
+            None
+        if exists(state_dim):
+            self.state_mlp = nn.Sequential(nn.LeakyReLU(negative_slope=0.1),nn.Linear(state_dim,dim_out))
+        else:
+            self.state_mlp = None
+        if res_unit:
+            self.res_block = Block(dim_in,dim_out) if dim_in != dim_out else nn.Identity()
+        else:
+            self.res_block = self.res_block = None
+        if norm:
+            self.normalization = nn.LayerNorm(dim_in)
+        else:
+            self.normalization = None
+        
+        
+    def forward(self,x,time=None,state=None):
+        if exists(self.normalization):
+            h = self.normalization(x) 
+        h = self.block1(h)
+        if exists(self.time_mlp) and exists(time):
+            time_emb = self.time_mlp(time)
+            h = h + time_emb
+        if exists(self.state_mlp) and exists(state):
+            state_emb = self.state_mlp(state)
+            h = h + state_emb   
+
+        h = self.block2(h)
+        if exists(self.res_block):
+            h = h + self.res_block(x)           
+        
+        return h      
+
+class Network(nn.Module):
+    def __init__(self,
+                 dim,
+                 hid_dims=(2,3),
+                 with_time_emb=True,
+                 with_domain = True,
+                 domain_dim = 16,
+                 with_state_condition=True,
+                ):
+        super().__init__()
+        
+        self.init_dim = dim
+        self.final_dim = dim
+        
+        if with_time_emb:  
+            time_dim = 256
+            self.time_mlp = nn.Sequential(
+                SinusoidalTimeEmbeddings(self.init_dim),
+                nn.Linear(self.init_dim,time_dim),
+                nn.LeakyReLU(negative_slope=0.1),
+                nn.Linear(time_dim,time_dim)
+            )  
+        else:
+            time_dim = None
+            self.time_mlp = None
+            
+        if with_state_condition:
+            state_dim = 256
+            self.state_mlp = nn.Sequential(
+                nn.Linear(self.init_dim,state_dim),
+                nn.LeakyReLU(negative_slope=0.1),
+                nn.Linear(state_dim,state_dim),
+                nn.LeakyReLU(negative_slope=0.1),
+                nn.Linear(state_dim,time_dim)
+                )
+        else:
+            state_dim = None
+            self.state_mlp = None    
+                   
+        if with_domain:
+            condition_dim = 256
+            self.graph_mlp = nn.Sequential(
+                nn.Linear(domain_dim,condition_dim),
+                nn.LeakyReLU(negative_slope=0.1),
+                nn.Linear(condition_dim,condition_dim),
+                nn.LeakyReLU(negative_slope=0.1),
+                nn.Linear(condition_dim,time_dim),
+            )
+            
+        self.init_block = Block(self.init_dim,hid_dims[0])
+        self.init_block.apply(init_weights)
+        self.blocks = nn.ModuleList([])
+        for (dim_in, dim_out) in list(zip(hid_dims[:-1],hid_dims[1:])):
+            self.blocks.append(
+                NetBlock(dim_in,dim_out,time_emb_dim=time_dim,state_dim=None,norm=True,res_unit=True)
+                )    
+        self.blocks.apply(init_weights)
+        self.final_block = Block(hid_dims[-1],self.final_dim)
+        self.final_block.apply(init_weights)   
+            
+    def forward(self,x,state,graph_condition,time):
+        t_diffusion = self.time_mlp(time)
+        graph = self.graph_mlp(graph_condition)
+        if exists(state) and exists(self.state_mlp):
+            state = self.state_mlp(state)
+            t = t_diffusion + graph + state
+        else:
+            t = t_diffusion + graph
+    
+        x = self.init_block(x)
+        for block in self.blocks:
+            x = block(x,t)
+        x = self.final_block(x)
+        return x
+
+# Defining diffusion process
+
+class Diffusion:
+
+    def __init__(self,diffusion_timesteps=100,beta_start=0.0001,beta_end=0.02):
+        self.diffusion_timesteps = diffusion_timesteps
+        x = torch.linspace(0,diffusion_timesteps,diffusion_timesteps+1)
+        x = torch.cos(x/diffusion_timesteps * torch.pi)
+        self.betas = beta_start + 0.5*(beta_end-beta_start)*(1-x)
+        self.alphas = 1. - self.betas
+        self.alphas_cumprod = torch.cumprod(self.alphas, axis=0)
+        self.alphas_cumprod_prev = F.pad(self.alphas_cumprod[:-1], (1, 0), value=1.0)
+        self.sqrt_recip_alphas = torch.sqrt(1.0 / self.alphas)
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.sqrt_alphas_cumprod = torch.sqrt(self.alphas_cumprod)
+        self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - self.alphas_cumprod)
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        self.posterior_variance = self.betas * (1. - self.alphas_cumprod_prev) / (1. - self.alphas_cumprod)
+
+    
+    # forward diffusion
+    def q_sample(self,x_start, t, noise=None):
+        if noise is None:
+            noise = torch.randn_like(x_start)
+
+        sqrt_alphas_cumprod_t = extract(self.sqrt_alphas_cumprod, t, x_start.shape)
+        sqrt_one_minus_alphas_cumprod_t = extract(
+            self.sqrt_one_minus_alphas_cumprod, t, x_start.shape
+        )
+        return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
 
 # Defining the whole model + training + sampling
 
-class GNNModel:
-    """ GNN-only ablated model. """
+# Class to create dataset from ABM ramifications and load all on device
+class ABMSampleDataset(torch.utils.data.Dataset):
+    def __init__(self, ramifications, n_ramifications, abm_featurizer, device):
+        self.device = device
+        self.samples = []
+        self.static_feature_dim = abm_featurizer.get_shape_state_features()[0]
 
-    def __init__(self, n_features, abm_featurizer: ABMFeaturizer,aggregation='add'):
+        assert len(ramifications[1]) >= n_ramifications
+
+        n_timesteps = len(ramifications) - 1
+        n_runs = n_ramifications
+
+        for t in range(n_timesteps):
+            prev_state = ramifications[t][0]
+            state_t0 = abm_featurizer.scale_abm_state(prev_state)
+            edge_index = abm_featurizer.get_interaction_graph(prev_state)
+            for run in range(n_runs):
+                next_state = ramifications[t+1][run]
+                state_t1 = abm_featurizer.scale_abm_state(next_state)
+                self.samples.append({
+                    "x_t": torch.tensor(state_t0, dtype=torch.float32),
+                    "edge_index": torch.tensor(edge_index, dtype=torch.long),
+                    "y_t": torch.tensor(state_t1[:, self.static_feature_dim:], dtype=torch.float32)
+                })
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        sample = self.samples[idx]
+        return (
+            sample["x_t"].to(self.device),
+            sample["edge_index"].to(self.device),
+            sample["y_t"].to(self.device)
+        )
+
+class NNModel_diffusion_only:
+    """ Our neural network model. """
+
+    def __init__(self, n_features, learning_rate, abm_featurizer: ABMFeaturizer, diffusion_timesteps, domain_dim):
+
         self.abm_featurizer = abm_featurizer
-        
-        self.abm_featurizer = abm_featurizer
-        self.feature_dim = n_features
+        self.feature_dim = n_features 
+
+        # Dimension of static features (e.g., agent type) placed at beginning of state vector
+        # Not seen by diffusion (self.static_feature_dim used for slicing)
+        # Seen in domain_condition
+
         self.static_feature_dim = self.abm_featurizer.get_shape_state_features()[0]
+
+        # Full state dimension (static + dynamic) seen by GNN
         self.state_dim = self.feature_dim + self.static_feature_dim
-        self.gnn_hidden_dims = [32,64,128,128,64,32]
-        self.lr_gnn = 0.0002
-        self.aggregation = aggregation
-        self.graph_model = GNN(self.state_dim,self.feature_dim,hidden=self.gnn_hidden_dims,aggregation=aggregation)
-        self.optimizer = Adam(self.graph_model.parameters(),lr=self.lr_gnn)
+        self.ld_hidden_dims = [128,256,1024,1024,256,128]
+        self.latent_dim = domain_dim
+        self.lr_ld = learning_rate
+        self.ld_model = Network(dim = self.feature_dim,
+                hid_dims=self.ld_hidden_dims,
+                with_time_emb=True,
+                with_domain=True,
+                domain_dim=self.latent_dim,
+                with_state_condition=True
+        )
+        self.diffusion_timesteps = diffusion_timesteps
+        self.optimizer1 = Adam(self.ld_model.parameters(),lr=self.lr_ld)
         self.losses = []
+        self.time = []
         
 
-    def p_losses(self,true_state, predicted_state,loss_type="l2"):
+    def p_losses(self,x_start,state,label,t,T,noise=None,loss_type="l2"):
+        if noise is None:
+            noise = torch.randn_like(x_start)
+        diffusion = Diffusion(diffusion_timesteps=T)
+        x_noisy = diffusion.q_sample(x_start=x_start, t=t, noise=noise)
+        predicted_noise = self.ld_model(x_noisy, state, label, t)
 
         if loss_type == 'l1':
-            loss = F.l1_loss(true_state, predicted_state)
+            loss = F.l1_loss(noise, predicted_noise)
         elif loss_type == 'l2':
-            loss = F.mse_loss(true_state, predicted_state)
+            loss = F.mse_loss(noise, predicted_noise)
         elif loss_type == "huber":
-            loss = F.smooth_l1_loss(true_state, predicted_state)
+            loss = F.smooth_l1_loss(noise, predicted_noise)
         else:
             raise NotImplementedError()
 
         return loss
 
-    def train(self, ramifications, n_epochs=10):
+    def train(self, ramifications, n_ramifications = 500, n_epochs=10):
         """ Train the model using a `ramifications` data set as generated by `generate_ramifications`. """
         
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.graph_model.to(device)
+        self.ld_model.to(device)
 
-        preprocessed_data = []  # list of (state_t0, edge_index, state_t1)
+        n_agents = ramifications[0][0].shape[0]
+        print('Transfering data to device:', device)
+        dataset = ABMSampleDataset(ramifications, n_ramifications, self.abm_featurizer, device)
+        print('Creating DataLoader')
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
+        self.time.append(time.time())
+        for epoch in tqdm(range(n_epochs), desc="Training"):
+            for x_t, edge_index, y_t in dataloader:
+                self.optimizer1.zero_grad()
 
-        n_timesteps = len(ramifications[1:])
-        n_runs = len(ramifications[1])
-        for t in range(n_timesteps):
-            prev_state = ramifications[t][0]
-            edge_index = torch.tensor(self.abm_featurizer.get_interaction_graph(prev_state), dtype=torch.long, device=device)
-            state_t0 = torch.tensor(self.abm_featurizer.scale_abm_state(prev_state), dtype=torch.float32, device=device)
-            for run in range(n_runs):
-                next_state = ramifications[t+1][run]
-                state_t1 = torch.tensor(self.abm_featurizer.scale_abm_state(next_state)[:, self.static_feature_dim:], dtype=torch.float32, device=device)
-        
-                preprocessed_data.append((state_t0, edge_index, state_t1))
+                # Diffusion takes only dynamic features (both prev_state and future state target)
+                prev_state_condition = x_t[0][:, self.static_feature_dim:]
+                # Domain condition takes full state (static + dynamic)
+                domain_condition = x_t[0].flatten().unsqueeze(0).expand(n_agents, -1)
 
-        pyg_graphs = []
-        for state_t0, edge_index, state_t1 in preprocessed_data:
-            data = Data(x=state_t0, edge_index=edge_index, y=state_t1)
-            pyg_graphs.append(data)
+                t_diffusion = torch.randint(
+                    0, self.diffusion_timesteps, (x_t.shape[1],), device=device
+                ).long()
 
-        loader = DataLoader(pyg_graphs, batch_size=16, shuffle=True)
-        
-        for epoch in tqdm(range(n_epochs),'Training:'):
-            for batch in loader:
-                self.optimizer.zero_grad()
-                out = self.graph_model(batch.x, batch.edge_index)
-                loss = self.p_losses(out, batch.y)
-                self.losses.append(loss.item())
+                loss = self.p_losses(
+                    x_start = y_t[0],
+                    state=prev_state_condition,
+                    label=domain_condition,
+                    t=t_diffusion,
+                    T=self.diffusion_timesteps,
+                    loss_type="l2"
+                )
+
                 loss.backward()
-                self.optimizer.step()
-        
+                self.optimizer1.step()
 
+                self.losses.append(loss.item())
+            self.time.append(time.time())
+
+
+    @torch.no_grad()
+    def p_sample(self,x,t,t_index,state,label):
+        diffusion = Diffusion(diffusion_timesteps=self.diffusion_timesteps)
+        betas_t = extract(diffusion.betas, t, x.shape)
+        sqrt_one_minus_alphas_cumprod_t = extract(
+            diffusion.sqrt_one_minus_alphas_cumprod, t, x.shape
+        )
+        sqrt_recip_alphas_t = extract(diffusion.sqrt_recip_alphas, t, x.shape)
+        model_mean = sqrt_recip_alphas_t * (
+            x - betas_t * self.ld_model(x,state,label,t) / sqrt_one_minus_alphas_cumprod_t
+        )
+        if t_index == 0:
+            return model_mean
+        else:
+            posterior_variance_t = extract(diffusion.posterior_variance, t, x.shape)
+            noise = torch.randn_like(x)
+            return model_mean + torch.sqrt(posterior_variance_t) * noise        
     
     @torch.no_grad()
     def next_step_samples(self, state: np.ndarray, n_samples=1) -> np.ndarray:
         """ Given a matrix representing the state at t, returns a list of possible outcomes,
             where each element of the list is a matrix representing the state at t+1.
         """
-        device = next(self.graph_model.parameters()).device
+        device = next(self.ld_model.parameters()).device
+        b = n_samples
 
-        edges = torch.tensor(self.abm_featurizer.get_interaction_graph(state)).to(device)
         state_array = self.abm_featurizer.scale_abm_state(state)
+        n_agents = state.shape[0]
+        domain = np.broadcast_to(state_array.flatten(),shape=(n_agents,state_array.flatten().shape[0]))
+        label = torch.tensor(domain).to(torch.float32).to(device)
         condition = torch.tensor(state_array).to(torch.float32).to(device)
-        outputs = []
-        for n in range(n_samples):
-            output = self.graph_model(condition,edges).cpu().numpy()
-            outputs.append(output)
-        outputs = np.array(outputs)
-        future_states = self.abm_featurizer.unscale_abm_state(outputs)
-        return np.array(future_states)
+        state_tensor = condition[:,2:].to(torch.float32).to(device)
+       
+        n, c = state_tensor.size()
+        samples = []
+        
+        for j in range(b):
+            sample = torch.randn((n,c), device=device,)
+            for i in reversed(range(0,self.diffusion_timesteps)):
+                sample = self.p_sample(sample, torch.full((n,), i, device=device, dtype=torch.long), i, state = state_tensor,label = label)
+            samples.append(sample.cpu().numpy())
+        samples = np.array(samples)
+        return self.abm_featurizer.unscale_abm_state(samples)
 
 
-class SimulatedABM_GNN_only(ABM):
+class SimulatedABM_diffusion_only(ABM):
     """ Class that takes a trained model and puts into the same interface used by ABM.
     """
-    def __init__(self, initial_state: np.ndarray, model: GNNModel):
+    def __init__(self, initial_state: np.ndarray, model: NNModel_diffusion_only):
         self.model = model
         self.initial_state_np = initial_state
 
